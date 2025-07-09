0a9c86b9245a61767c38418c5035beb065945977
Viana-16 <163910372+Viana-16@users.noreply.github.com>
2025-07-09 17:06:19 -0300

    09-07

--- LojaMoveis/Services/ClienteService.cs
+++ LojaMoveis/Services/ClienteService.cs
@@ -17,11 +17,45 @@ public class ClienteService
         _clienteCollection = mongoDatabase.GetCollection<Cliente>(settings.Value.ClienteCollectionName);
     }
 
+
+
     // Método para buscar um cliente pelo e-mail
     public async Task<Cliente?> GetByEmailAsync(string email)
     {
         return await _clienteCollection.Find(c => c.Email == email).FirstOrDefaultAsync();
     }
+
+    //Validação pra ver se CPF é real
+    private bool ValidarCPF(string cpf)
+    {
+        cpf = new string(cpf.Where(char.IsDigit).ToArray());
+        if (cpf.Length != 11 || cpf.Distinct().Count() == 1)
+            return false;
+
+        var multiplicadores1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+        var multiplicadores2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+        var tempCpf = cpf.Substring(0, 9);
+        var soma = 0;
+
+        for (int i = 0; i < 9; i++)
+            soma += int.Parse(tempCpf[i].ToString()) * multiplicadores1[i];
+
+        var resto = soma % 11;
+        var digito1 = resto < 2 ? 0 : 11 - resto;
+
+        tempCpf += digito1;
+        soma = 0;
+
+        for (int i = 0; i < 10; i++)
+            soma += int.Parse(tempCpf[i].ToString()) * multiplicadores2[i];
+
+        resto = soma % 11;
+        var digito2 = resto < 2 ? 0 : 11 - resto;
+
+        return cpf.EndsWith($"{digito1}{digito2}");
+    }
+
 
     // Método para pegar todos os clientes
     public async Task<List<Cliente>> GetAllAsync() =>
@@ -39,15 +73,15 @@ public class ClienteService
         if (existingEmail != null)
             throw new Exception("E-mail já cadastrado.");
 
+        if (!ValidarCPF(cliente.Cpf))
+            throw new Exception("CPF inválido. Por favor, insira um CPF válido.");
+
         // Verifica se já existe cliente com mesmo CPF
         var existingCpf = await _clienteCollection.Find(c => c.Cpf == cliente.Cpf).FirstOrDefaultAsync();
         if (existingCpf != null)
             throw new Exception("CPF já cadastrado.");
 
-        // Verifica se já existe cliente com mesmo telefone
-        var existingTelefone = await _clienteCollection.Find(c => c.Telefone == cliente.Telefone).FirstOrDefaultAsync();
-        if (existingTelefone != null)
-            throw new Exception("Telefone já cadastrado.");
+
 
         // Criptografa a senha antes de salvar
         cliente.Senha = BCrypt.Net.BCrypt.HashPassword(cliente.Senha);
